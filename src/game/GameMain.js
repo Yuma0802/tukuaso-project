d8b605705672d06e6a580014a4325edaf1fbc3df
KojimaNaoyuki <programkoji213@gmail.com>
2022-08-18 23:55:22 +0900

    create random code monk

--- src/game/GameMain.js
+++ src/game/GameMain.js
@@ -6,12 +6,35 @@ import GameView from "./GameView";
 class GameMain extends Component {
     constructor(props) {
         super(props);
+
+        this.maxGameViewX = 500;
+        this.maxGameViewY = 500;
+
+        this.state = {
+            monkCoordinate: this.createRandomCoordinate(this.maxGameViewX, this.maxGameViewY)
+        }
     }
+
+    /////////////////////////////////////
+    //game script
+    // componentDidMount() {
+    //     this.setMonkCoordinate();
+    // }
+
+
+    createRandomCoordinate(maxX, maxY) {
+        let x = Math.floor( Math.random() * (maxX + 1 - 0) ) + 0;
+        let y = Math.floor( Math.random() * (maxY + 1 - 0) ) + 0;
+        return [x, y]
+    }
+
+
+    /////////////////////////////////////
 
     render() {
         return(
             <>
-                <GameView />
+                <GameView maxGameViewX={this.maxGameViewX} maxGameViewY={this.maxGameViewY} monkCoordinate={this.state.monkCoordinate} />
             </>
         );
     }
