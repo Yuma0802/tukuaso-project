74f3009c66e138159c6003c201105dae041947c9
Yuma0802 <71355267+Yuma0802@users.noreply.github.com>
2022-08-20 01:57:24 +0900

    #23 done

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import GameMain from './game/GameMain';
-import TitlePage from './presentational/Organisms/TitlePage';
+//import TitlePage from './presentational/Organisms/TitlePage';
+import CrearOrOutTemplate from './Presentational/Organisms/CrearOrOutTemplate';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/'><TitlePage /></Route>
-        <Route exact path='/game/:lv'><GameMain /></Route>
-      </Switch>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Switch>
+    //     <Route exact path='/'><TitlePage /></Route>
+    //     <Route exact path='/game/:lv'><GameMain /></Route>
+    //   </Switch>
+    // </BrowserRouter>
+
+    <CrearOrOutTemplate num={3} text="やりなおす"/>
   );
 }
 
