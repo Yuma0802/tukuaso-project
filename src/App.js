ad282c407f257d96c3d8ba50ba9f80c2fb7f0601
KojimaNaoyuki <programkoji213@gmail.com>
2022-08-20 02:30:04 +0900

    fix maerge

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import GameMain from './game/GameMain';
-//import TitlePage from './presentational/Organisms/TitlePage';
-import ClearOrOutTemplate from './Presentational/Organisms/ClearOrOutTemplate';
+import TitlePage from './Presentational/Organisms/TitlePage';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Switch>
-    //     <Route exact path='/'><TitlePage /></Route>
-    //     <Route exact path='/game/:lv'><GameMain /></Route>
-    //   </Switch>
-    // </BrowserRouter>
-
-    <ClearOrOutTemplate num={2} text="つぎへ"/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'><TitlePage /></Route>
+        <Route exact path='/game/:lv'><GameMain /></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
